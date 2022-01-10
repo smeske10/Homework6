143ee5c47f797e27e2e488d3a2b045b037c57d5c
smeske10 <35307525+smeske10@users.noreply.github.com>
2022-01-09 20:38:42 -0500

    Added javascript functions for API and event listeners for buttons
    Functions: Search city by user input, API Call to get weather, Display weather on HTML

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,3 +7,45 @@ var wind = document.getElementById('wind');
 var humidity = document.getElementById('humidity');
 var uv = document.getElementById('uv');
 
+window.onload(getWeather());
+
+//Search city by user input 
+var buttonClickHandler = function (event) {
+    var searchCity = event.target.getAttribute(search.value);
+  
+    if (searchCity) {
+      getWeather(searchCity);
+    }
+    searchHistory.textContent = '';
+    var searchHistoryEl = searchHistory.createElement('button')
+    searchHistoryEl.classList = "searchBtn btn-secondary"
+}
+
+//API Call to get weather
+var getWeather = function (weather) {
+    var apiUrl = 'https://api.openweathermap.org/data/2.5/onecall?' + searchCity + '&exclude=hourly,daily&appid={API key}';
+        
+    // if (searchCity = '') {
+    //     var defaultCity = 'Philadelphia'
+    // }
+
+    fetch(apiUrl).then(function(response) {
+        if(response.ok) {
+            response.json().then(function(data) {
+                console.log(data);
+                displayWeather(data, weather);
+            });
+        }
+    })
+}
+
+//Display weather on HTML
+var displayWeather = function (weatherForecast, search) {
+    if (weatherForecast.length === 0) {
+        city.textContent = 'City not found';
+        return;
+    }
+};
+
+search.addEventListener('click', buttonClickHandler);
+history.addEventListener('click', buttonClickHandler)
