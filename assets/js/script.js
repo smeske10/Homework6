1a037e04657aafdafb5a727bc03ca677633b09a2
smeske10 <35307525+smeske10@users.noreply.github.com>
2022-01-09 20:21:37 -0500

    Created HTML, CSS, JS
    Built basic website layout and defined DOM elements in script.js

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+//DOM Elements
+var searchHistory = document.querySelector('#history');
+var search = document.querySelector('#search');
+var city = document.getElementById('city');
+var temp = document.getElementById('temp');
+var wind = document.getElementById('wind');
+var humidity = document.getElementById('humidity');
+var uv = document.getElementById('uv');
+
