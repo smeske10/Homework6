1d3d54a4a767e9f1ca5869547ccfc89d43b03c96
smeske10 <35307525+smeske10@users.noreply.github.com>
2022-01-10 22:27:04 -0500

    Added API functionality, local storage of search history and create element button for history

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,51 +1,93 @@
 //DOM Elements
 var searchHistory = document.querySelector('#history');
 var search = document.querySelector('#search');
+var searchBtn = document.querySelector('.searchBtn');
 var city = document.getElementById('city');
 var temp = document.getElementById('temp');
 var wind = document.getElementById('wind');
 var humidity = document.getElementById('humidity');
 var uv = document.getElementById('uv');
 
-window.onload(getWeather());
+document.addEventListener('DOMContentLoaded', getWeather);
 
-//Search city by user input 
-var buttonClickHandler = function (event) {
-    var searchCity = event.target.getAttribute(search.value);
+//Submitted City collected
+var formSubmitHandler = function (event) {
+    event.preventDefault();
+  
+    var searchCity = search.value.trim();
   
     if (searchCity) {
       getWeather(searchCity);
+  
+      searchHistory.textContent = '';
+      search.textContent = '';
+    } else {
+      alert('Please enter a GitHub username');
     }
-    searchHistory.textContent = '';
-    var searchHistoryEl = searchHistory.createElement('button')
-    searchHistoryEl.classList = "searchBtn btn-secondary"
+};
+
+//Search city by user input 
+var buttonClickHandler = function (event) {
+
+    var searchCityHistory = event.target.getAttribute(search.value);
+  
+    if (searchCityHistory) {
+      getWeather(localStorage.getItem('cityHistory'));
+
+      searchHistory.textContent = '';
+    }
 }
 
 //API Call to get weather
 var getWeather = function (weather) {
-    var apiUrl = 'https://api.openweathermap.org/data/2.5/onecall?' + searchCity + '&exclude=hourly,daily&appid={API key}';
-        
-    // if (searchCity = '') {
-    //     var defaultCity = 'Philadelphia'
-    // }
+    var apiUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid=6b1721c2618cf51580986feecb262fb5'
 
-    fetch(apiUrl).then(function(response) {
-        if(response.ok) {
-            response.json().then(function(data) {
-                console.log(data);
-                displayWeather(data, weather);
-            });
-        }
+    fetch(apiUrl)
+        .then(function(response) {
+            if(response.ok) {
+                response.json().then(function(data) {
+                    console.log(data);
+                    displayWeather(data, weather);
+                    displayHistory();
+                });
+            } else {
+                alert('Error: ' + response.statusText);
+              }
     })
-}
+        .catch(function (error) {
+            alert('Unable to connect to OpenWeather');
+        });
+};
 
-//Display weather on HTML
-var displayWeather = function (weatherForecast, search) {
+var displayHistory = function () {
+
+    var searchHistoryEl = document.createElement('button')
+
+    searchHistoryEl.classList = "btn-secondary searchBtn";
+    searchHistoryEl.setAttribute('button', search.value);
+
+    searchHistoryEl.innerHTML = search.value;
+
+    searchHistory.appendChild(searchHistoryEl);
+
+    localStorage.setItem('cityHistory', search.value)
+};
+
+var displayWeather = function (weatherForecast) {
     if (weatherForecast.length === 0) {
         city.textContent = 'City not found';
         return;
     }
+
+    // var searchHistoryEl = document.createElement('button')
+
+    // searchHistoryEl.classList = "btn-secondary searchBtn";
+    // searchHistoryEl.setAttribute('button', search.value);
+
+    // searchHistoryEl.innerHTML = search.value;
+    
+    // searchHistory.appendChild(searchHistoryEl);
 };
 
-search.addEventListener('click', buttonClickHandler);
-history.addEventListener('click', buttonClickHandler)
+searchBtn.addEventListener('click', formSubmitHandler);
+searchHistory.addEventListener('click', buttonClickHandler);
